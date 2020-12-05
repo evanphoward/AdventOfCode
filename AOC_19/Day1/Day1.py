8b2a84770afa55c0e74d6a5a9b53e8b89b14b8c2
Evan Howard <evanhoward@college.harvard.edu>
2020-12-05 15:13:15 -0500

    Improving 2019 Day 1

--- AOC_19/Day1/Day1.py
+++ AOC_19/Day1/Day1.py
@@ -1,7 +1,10 @@
+print("Part 1:", sum([int(x) // 3 - 2 for x in open("input").readlines()]))
+
 total = 0
 for mass in open("input").readlines():
-    curr_mass = int(mass)
-    curr_mass = curr_mass // 3
-    curr_mass -= 2
-    total += curr_mass
-print(total)
+    mass = int(mass)
+    while mass > 0:
+        mass = mass // 3 - 2
+        if mass > 0:
+            total += mass
+print("Part 2:", total)
