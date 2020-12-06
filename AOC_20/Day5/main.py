84587824c66458fb2a6f5e8c3e18fc26a22183e9
Evan Howard <evanhoward@college.harvard.edu>
2020-12-06 18:00:20 -0500

    Much better 2020 Day 5

--- AOC_20/Day5/main.py
+++ AOC_20/Day5/main.py
@@ -1,21 +1,4 @@
-import math
-
-seats = list()
-for line in open("input").readlines():
-    min_r = 0
-    max_r = 127
-    min_c = 0
-    max_c = 7
-    for ch in [char for char in line]:
-        if ch == "F":
-            max_r = (min_r + max_r) // 2
-        elif ch == "B":
-            min_r = math.ceil((min_r + max_r) / 2)
-        elif ch == "R":
-            min_c = math.ceil((min_c + max_c) / 2)
-        elif ch == "L":
-            max_c = (min_c + max_c) // 2
-    seats.append(8*min_r+min_c)
+seats = [int(str(''.join(['1' if ch in {"R", "B"} else '0' for ch in line.strip()])), 2) for line in open("input").readlines()]
 
 print("Part 1:", max(seats))
 for i in range(min(seats), max(seats) + 1):
