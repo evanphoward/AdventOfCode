176f9cd35fa07e884fc51d3350145fa7537fc061
Evan Howard <evanhoward@college.harvard.edu>
2020-12-10 00:18:38 -0500

    2020 Day 10 Solution. 81/409. First leaderboard!

---
+++ AOC_20/Day10/main.py
@@ -0,0 +1,16 @@
+adapters = sorted([int(line.strip()) for line in open("input").readlines()] + [0])
+adapters.append(max(adapters) + 3)
+
+ones = sum([1 for i in range(1, len(adapters)) if adapters[i] - adapters[i - 1] == 1])
+threes = sum([1 for i in range(1, len(adapters)) if adapters[i] - adapters[i - 1] == 1])
+print("Part 1:", ones * threes)
+
+ways = [0]*(len(adapters))
+
+ways[0] = 1
+for i in range(1, len(adapters)):
+    for j in range(0, i):
+        if adapters[i] - adapters[j] <= 3:
+            ways[i] += ways[j]
+
+print("Part 2:", ways[-1])
