7416a2df9a0e141e794b6a8cdd0de45716e34dd1
Evan Howard <evanhoward@college.harvard.edu>
2020-12-20 15:51:59 -0500

    2020 Day 10 Fix

--- AOC_20/Day10/main.py
+++ AOC_20/Day10/main.py
@@ -2,7 +2,7 @@ adapters = sorted([int(line.strip()) for line in open("input").readlines()] + [0
 adapters.append(max(adapters) + 3)
 
 print("Part 1:", sum([adapters[i] - adapters[i - 1] == 1 for i in range(1, len(adapters))]) *
-      sum([adapters[i] - adapters[i - 1] == 1 for i in range(1, len(adapters))]))
+      sum([adapters[i] - adapters[i - 1] == 3 for i in range(1, len(adapters))]))
 
 ways = [0]*(len(adapters))
 ways[0] = 1
