f500ce70eb81b3ef97438ac93bd9bd5d48ce2312
Evan Howard <evanhoward@college.harvard.edu>
2021-12-05 11:12:13 -0500

    2016 Day 20, Use sets in 2021 Day 5

--- AOC_21/Day5/main.py
+++ AOC_21/Day5/main.py
@@ -1,5 +1,6 @@
 def fill_pipes(p1):
-    lines = [[0] * 1000 for _ in range(1000)]
+    seen_twice = set()
+    seen = set()
     for pipe in inp:
         x1, y1 = pipe[0][0], pipe[0][1]
         x2, y2 = pipe[1][0], pipe[1][1]
@@ -9,10 +10,13 @@ def fill_pipes(p1):
         if p1 and (dx != 0 and dy != 0):
             continue
         for i in range(steps + 1):
-            lines[x1 + (i * dx)][y1 + (i * dy)] += 1
-    return lines
+            new_pos = (x1 + (i * dx), y1 + (i * dy))
+            if new_pos in seen:
+                seen_twice.add(new_pos)
+            seen.add(new_pos)
+    return len(seen_twice)
 
 
 inp = [[[int(x) for x in pos.split(",")] for pos in line.strip().split(" -> ")] for line in open("input").readlines()]
-print("Part 1:", sum(sum(p >= 2 for p in row) for row in fill_pipes(True)))
-print("Part 2:", sum(sum(p >= 2 for p in row) for row in fill_pipes(False)))
+print("Part 1:", fill_pipes(True))
+print("Part 2:", fill_pipes(False))
