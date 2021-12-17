78b0f0779a73f09afae273a46fe69a3c04b0aaf7
Evan <evan.p.howard@gmail.com>
2021-12-17 00:38:47 -0500

    Clean up 2021 Day 17

--- AOC_21/Day17/main.py
+++ AOC_21/Day17/main.py
@@ -1,17 +1,6 @@
-def step(x, y, dx, dy):
-    x += dx
-    y += dy
-    if dx < 0:
-        dx += 1
-    elif dx > 0:
-        dx -= 1
-    dy -= 1
-    return x, y, dx, dy
-
-
-bounds = [bound[2:] for i, bound in enumerate(open("input").readline().replace(",", "").split()) if i in (2, 3)]
-min_x, max_x = map(int, bounds[0].split(".."))
-min_y, max_y = map(int, bounds[1].split(".."))
+bounds = open("input").readline().replace(",","").split()
+min_x, max_x = map(int, bounds[2][2:].split(".."))
+min_y, max_y = map(int, bounds[3][2:].split(".."))
 highest = []
 for vel_x in range(max_x + 1):
     for vel_y in range(min_y, 300):
@@ -20,14 +9,12 @@ for vel_x in range(max_x + 1):
         while x <= max_x and y >= min_y:
             x += dx
             y += dy
-            if dx < 0:
-                dx += 1
-            elif dx > 0:
-                dx -= 1
+            dx += 1 if dx < 0 else -1 if dx > 0 else 0
             dy -= 1
-            highest_y = max(y, highest_y)
+            highest_y = y if dy >= 0 else highest_y
             if min_y <= y <= max_y and min_x <= x <= max_x:
                 highest.append(highest_y)
                 break
+
 print("Part 1:", max(highest))
 print("Part 2:", len(highest))
