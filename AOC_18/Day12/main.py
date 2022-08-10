7e4c86d13b36adff1122758a6c999902c58e2bd8
Evan Howard <ehoward@roblox.com>
2022-08-10 11:26:06 -0700

    2018 day 12 part 2, day 13 work

--- AOC_18/Day12/main.py
+++ AOC_18/Day12/main.py
@@ -7,12 +7,23 @@ pots = defaultdict(bool)
 for i, pot in enumerate(initial_state.split()[2]):
     pots[i] = pot == "#"
 
-
-for _ in range(20):
+prev_sum = sum(pot_num if pot_val else 0 for pot_num, pot_val in pots.items())
+prev_delta = -1
+for i in range(10000):
     new_pots = defaultdict(bool)
-    for i in range(min(pots.keys()) - 2, max(pots.keys()) + 3):
-        pattern = ''.join("#" if pots[j] else "." for j in range(i - 2, i + 3))
-        new_pots[i] = rules[pattern] == "#"
+    for j in range(min(pots.keys()) - 2, max(pots.keys()) + 3):
+        pattern = ''.join("#" if pots[k] else "." for k in range(j - 2, j + 3))
+        new_pots[j] = rules[pattern] == "#"
     pots = new_pots
 
-print("Part 1:", sum(pot_num if pot_val else 0 for pot_num, pot_val in pots.items()))
+    if i == 19:
+        print("Part 1:", sum(pot_num if pot_val else 0 for pot_num, pot_val in pots.items()))
+
+    pot_sum = sum(pot_num if pot_val else 0 for pot_num, pot_val in pots.items())
+    delta = pot_sum - prev_sum
+    if delta == prev_delta:
+        print("Part 2:", pot_sum + (delta * (50000000000 - i - 1)))
+        break
+    prev_sum = pot_sum
+    prev_delta = delta
+    
