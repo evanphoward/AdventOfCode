9792b21b910286c487b23bce0a02198169e33f57
Evan Howard <ehoward@roblox.com>
2022-08-10 11:26:06 -0700

    2018 day 12 part 2, day 13 work

--- AOC_18/Day13/main.py
+++ AOC_18/Day13/main.py
@@ -1,18 +1,17 @@
-def compare_carts(cart1, cart2):
-    cart1_r, cart1_c, _ = cart1
-    cart2_r, cart2_c, _ = cart2
+from functools import cmp_to_key
 
-    if cart1_r < cart2_r:
-        return -1
-    elif cart1_r > cart2_r:
-        return 1
-    else:
-        if cart1_c < cart2_c:
-            return -1
-        elif cart1_c > cart2_c:
-            return 1
-        else:
-            return 0
+def print_carts(cts):
+    cart_pos = dict()
+    for r, c, heading, _ in cts:
+        cart_pos[(r, c)] = heading
+    for r in range(65, 75):
+        for c in range(5, 15):
+            if (r, c) in cart_pos:
+                print(['^', '>', 'v', '<'][cart_pos[(r, c)]], end='')
+            else:
+                print(map[r][c], end='')
+        print()
+    print()
 
 map = open("input").read().split("\n")
 WIDTH = len(map[0])
@@ -21,43 +20,40 @@ carts = []
 for r in range(HEIGHT):
     for c in range(WIDTH):
         if map[r][c] == '^':
-            carts.append((r, c, 0))
+            carts.append((r, c, 0, 0))
         elif map[r][c] == '>':
-            carts.append((r, c, 1))
+            carts.append((r, c, 1, 0))
         elif map[r][c] == 'v':
-            carts.append((r, c, 2))
+            carts.append((r, c, 2, 0))
         elif map[r][c] == '<':
-            carts.append((r, c, 3))
+            carts.append((r, c, 3, 0))
 
 correct_mapping = {'|': '|', '-': '-', '/': '/', '\\': '\\', '^': '|', 'v': '|', '<': '-', '>': '-', ' ': ' ', '+': '+'}
 map = [[correct_mapping[cell] for cell in line] for line in map]
+heading_delta = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 while True:
     new_carts = []
     pos_occupied = set()
-    for r, c, heading in carts:
-        if c < 0:
-            print(r, c, heading)
-        # print(r, c, heading)
-        if heading == 0:
-            r -= 1
-            if map[r][c] == '/':
-                heading = 1
-        elif heading == 1:
-            c += 1
-            if map[r][c] == '\\':
-                heading = 2
-        elif heading == 2:
-            r += 1
-            if map[r][c] == '/':
-                heading = 3
-        elif heading == 3:
-            c -= 1
-            if map[r][c] == '\\':
-                heading = 0
+    for r, c, heading, turn in carts:
+        dr, dc = heading_delta[heading]
+        r += dr
+        c += dc
+        if map[r][c] == '/':
+            heading = 3 - ((heading + 2) % 4)
+        elif map[r][c] == '\\':
+            heading = 3 - heading
+        elif map[r][c] == '+':
+            if turn == 0:
+                heading = (heading - 1) % 4
+            elif turn == 2:
+                heading = (heading + 1) % 4
+            turn = (turn + 1) % 3
+
         if (r, c) in pos_occupied:
-            print("Part 1:", "(" + r + ", " + c + ")")
+            print("Part 1:", "(" + str(c) + ", " + str(r) + ")")
             exit()
-        new_carts.append((r, c, heading))
+        new_carts.append((r, c, heading, turn))
         pos_occupied.add((r, c))
-    carts = sorted(new_carts, cmp=compare_carts)
+    carts = sorted(new_carts, key=lambda c: (c[0], c[1]))
+    print(carts)
