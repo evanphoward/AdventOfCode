4810fc686a735a3ee43dd9777bf769270c7f0d19
Evan Howard <evanhoward@college.harvard.edu>
2023-12-15 14:21:27 -0500

    Complete but slow 2018 Day 17

--- AOC_18/Day17/main.py
+++ AOC_18/Day17/main.py
@@ -1,73 +1,71 @@
-from collections import defaultdict
-import sys
-sys.setrecursionlimit(10000)
+inp = open("input").read().split("\n")
 
-s = open("input").read().strip()
-s = s.split("\n")
-
-grid = defaultdict(lambda : '.')
-min_y, max_y = 0, 0
-for wall in s:
+walls = set()
+min_y, max_y = 1000, 0
+min_x, max_x = 500, 500
+for wall in inp:
     if wall[0] == "x":
         wall = wall.split(", y=")
         wall_x = int(wall[0][2:])
         wall_ys = [int(x) for x in wall[1].split("..")]
         min_y = min(min_y, wall_ys[0])
         max_y = max(max_y, wall_ys[1])
+        min_x = min(min_x, wall_x)
+        max_x = max(max_x, wall_x)
         for y in range(wall_ys[0], wall_ys[1] + 1):
-            grid[(wall_x, y)] = "#"
+            walls.add((wall_x, y))
     else:
         wall = wall.split(", x=")
         wall_y = int(wall[0][2:])
         wall_xs = [int(x) for x in wall[1].split("..")]
+        min_x = min(min_x, wall_xs[0])
+        max_x = max(max_x, wall_xs[1])
+        min_y = min(min_y, wall_y)
+        max_y = max(max_y, wall_y)
         for x in range(wall_xs[0], wall_xs[1] + 1):
-            grid[(x, wall_y)] = "#"
+            walls.add((x, wall_y))
+flow = {(500, 0)}
+water = set()
+all_things = flow | walls
+obstacles = walls.copy()
+
+while True:
+    new_water, new_flow = water.copy(), set()
+    for x, y in flow:
+        if (x, y) not in new_water:
+            new_flow.add((x, y))
+        if y == max_y:
+            continue
+        i = 1
+        while (x, y + i) not in all_things and y + i <= max_y:
+            new_flow.add((x, y + i))
+            i += 1
+        if (x, y + 1) in walls or (x, y + 1) in water:
+            if (x + 1, y) not in all_things:
+                new_flow.add((x + 1, y))
+            if (x - 1, y) not in all_things:
+                new_flow.add((x - 1, y))
+        if (x - 1, y) in walls and (x, y + 1) in obstacles:
+            i = 1
+            while (x + i, y) in flow and (x + i, y + 1) in obstacles:
+                i += 1
+            if (x + i, y) in walls:
+                while i > 0:
+                    new_water.add((x + i - 1, y))
+                    if (x + i - 1, y) in new_flow:
+                        new_flow.remove((x + i - 1, y))
+                    i -= 1
+
+    if flow == new_flow and water == new_water:
+        break
+    flow = new_flow
+    water = new_water
+    all_things = (flow | water | walls)
+    obstacles = (water | walls)
 
 
-SAND = "."
-WALL = "#"
-WATER = "~"
-FLOW = "|"
-VOID = " "
-
-def fill(x, y, direction):
-    if grid[(x, y)] == FLOW:
-        fill(x + direction, y, direction)
-        grid[(x, y)] = WATER
-
-def flow(x, y, direction):
-    global grid
-    if y > max_y:
-        return True
-    elt = grid[(x, y)]
-    if elt != SAND:
-        return elt != WALL and elt != WATER
-
-    grid[(x, y)] = FLOW
-
-    leaks = flow(x, y + 1, 0)
-    if leaks:
-        return True
-
-    leaks_left = direction in (-1, 0) and flow(x - 1, y, -1)
-    leaks_right = direction in (0, 1) and flow(x + 1, y, 1)
-    leaks = leaks_left or leaks_right
-    if leaks:
-        return True
-
-    if direction == 0:
-        fill(x, y, -1)
-        fill(x + 1, y, 1)
-
-    return False
-
-
-flow(500, 0, 0)
-ans = 0
-for elt in grid.values():
-    if elt not in ("#", "."):
-        ans += 1
-print(ans - min_y - 1)
+print("Part 1:", len(water) + len(flow) - min_y)
+print("Part 2:", len(water))
 
 
 
