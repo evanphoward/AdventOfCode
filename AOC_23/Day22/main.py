64d8e724924930381ab7c89abdcf4ab285202dae
Evan Howard <evanhoward@college.harvard.edu>
2023-12-23 20:07:14 -0500

    2023 Day 22

--- AOC_23/Day22/main.py
+++ AOC_23/Day22/main.py
@@ -1,8 +1,5 @@
 import functools
-from collections import defaultdict, deque
-
-inp = open("input").read().split("\n")
-
+from collections import defaultdict
 
 def get_points_from_pos(init_pos, final_pos):
     points = set()
@@ -25,15 +22,11 @@ class Brick:
         self.i = i
 
     def drop(self, settled_blocks):
-        if any(x[2] == 1 for x in self.points):
-            return False
-
-        new_points = {(x[0], x[1], x[2] - 1) for x in self.points}
-        if new_points.intersection(settled_blocks):
-            return False
-
-        self.points = new_points
-        return True
+        while True:
+            new_points = {(x[0], x[1], x[2] - 1) for x in self.points}
+            if any(x[2] == 0 for x in new_points) or new_points.intersection(settled_blocks):
+                break
+            self.points = new_points
 
     def supported(self, all_bricks):
         supported_by = set()
@@ -45,31 +38,22 @@ class Brick:
                 supported_by.add(brick)
         return supported_by
 
-    def __repr__(self):
-        return str(self.i) + ": " + str(self.points)
-
     def __lt__(self, other):
         return min(x[2] for x in self.points) < min(x[2] for x in other.points)
 
-ans = 0
+
 bricks = []
-for i, line in enumerate(inp):
+for i, line in enumerate(open("input").read().split("\n")):
     init_pos, final_pos = line.split("~")
     init_pos = tuple(map(int, init_pos.split(",")))
     final_pos = tuple(map(int, final_pos.split(",")))
     bricks.append(Brick(i, get_points_from_pos(init_pos, final_pos)))
 bricks.sort()
-dropped = True
+
 settled_pts = set()
-while dropped:
-    dropped = False
-    for brick in bricks:
-        settled_pts = settled_pts.difference(brick.points)
-        if brick.drop(settled_pts):
-            dropped = True
-        else:
-            settled_pts = settled_pts.union(brick.points)
-bricks.sort()
+for brick in bricks:
+    brick.drop(settled_pts)
+    settled_pts = settled_pts.union(brick.points)
 
 ans = set()
 graph = defaultdict(set)
@@ -81,7 +65,8 @@ for brick in bricks:
         ans.add(list(supported_by)[0])
 print("Part 1:", len(bricks) - len(ans))
 
-def count(brick, graph):
+ans = 0
+for brick in bricks:
     indeg = defaultdict(int)
     for t_brick in bricks:
         for in_brick in graph[t_brick]:
@@ -96,6 +81,5 @@ def count(brick, graph):
             if indeg[i] == 0:
                 q.append(i)
 
-    return count
-
-print("Part 2:", sum(count(brick, graph) for brick in bricks))
+    ans += count
+print("Part 2:", ans)
