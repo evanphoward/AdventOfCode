17f53dc8416b18cb90159823133b08c0fcad77f0
Evan Howard <evanhoward@college.harvard.edu>
2023-12-31 15:15:48 -0500

    Remove extra fxn in 2023 Day 7

--- AOC_23/Day7/main.py
+++ AOC_23/Day7/main.py
@@ -18,23 +18,13 @@ def get_hand_type(hand, p2):
     if 4 in hand_count.values():
         return 1
     if 3 in hand_count.values():
-        if 2 in hand_count.values():
-            return 2
-        return 3
+        return 2 if 2 in hand_count.values() else 3
     if 2 in hand_count.values():
-        twos = 0
-        for x in hand_count.values():
-            if x == 2:
-                twos += 1
-        if twos == 2:
-            return 4
-        return 5
+        return 4 if Counter(hand_count.values())[2] == 2 else 5
     return 6
 
 
 def comp(hand_1, hand_2, p2):
-    hand_1 = hand_1[0]
-    hand_2 = hand_2[0]
     h1 = get_hand_type(hand_1, p2)
     h2 = get_hand_type(hand_2, p2)
     if h1 > h2:
@@ -49,28 +39,12 @@ def comp(hand_1, hand_2, p2):
     return 0
 
 
-def reassign_jacks(hands):
-    new_hands = []
-
-    for hand, bid in hands:
-        hand_count = Counter(hand)
-        to_swap = hand_count.most_common()[0][0]
-        if to_swap == 11 and len(hand_count) > 1:
-            to_swap = hand_count.most_common()[1][0]
-
-        new_hands.append((tuple(x if x != 11 else to_swap for x in hand), bid))
-
-    return new_hands
-
-
 def run(p2):
-    mappings = {'J': 11, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
-                '7': 7, '8': 8, '9': 9, 'T': 10, 'Q': 12, 'K': 13, 'A': 14}
-    if p2:
-        mappings['J'] = 0
+    mappings = {'J': 0 if p2 else 11, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
+            '7': 7, '8': 8, '9': 9, 'T': 10, 'Q': 12, 'K': 13, 'A': 14}
     inp = [(tuple(mappings[y] for y in x.split()[0]), int(x.split()[1])) for x in open("input").read().split("\n")]
-    inp = sorted(inp, key=cmp_to_key(lambda h1, h2: comp(h1, h2, p2)), reverse=True)
-    return sum(bid * (i + 1) for i, (_, bid) in enumerate(inp))
+    return sum(bid * (i + 1) for i, (_, bid) in enumerate(sorted(inp, key=cmp_to_key(lambda h1, h2: comp(h1[0], h2[0], p2)), reverse=True)))
 
-print("Patt 1:", run(False))
+
+print("Part 1:", run(False))
 print("Part 2:", run(True))
