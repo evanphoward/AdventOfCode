3a94933c4bc79e62ec384cf4dad27858e570f22d
Evan Howard <evanhoward@college.harvard.edu>
2024-11-24 13:02:09 -0500

    Modify get_input to not remove whitespace at beginning of input

--- MiscFiles/library.py
+++ MiscFiles/library.py
@@ -22,7 +22,7 @@ def get_input(year, day):
     except FileNotFoundError:
         target_url = 'https://www.adventofcode.com/' + str(year) + '/day/' + str(day) + '/input'
         session_key = open("../../MiscFiles/session-key").read().strip()
-        response = requests.get(target_url, cookies={'session':session_key}).text.strip()
+        response = requests.get(target_url, cookies={'session':session_key}).text.rstrip()
         open("input", "w").write(response)
         return response
 
