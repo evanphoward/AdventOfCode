0c8370a8e62ba37a1d44b4813cf2436c3aa48a6e
Evan Howard <evanhoward@college.harvard.edu>
2024-12-01 00:15:25 -0500

    2024 Day 1. 62nd / 30th!

--- AOC_24/Day1/main.py
+++ AOC_24/Day1/main.py
@@ -1,10 +1,16 @@
 from MiscFiles.library import *
 
-inp = get_input(0, 0).split("\n")
-inp = [int(x) for x in inp]
-ans = 0
+inp = get_input(2024, 1).split("\n")
 
-for x in inp:
-    continue
+left_list, right_list = [], []
+for line in inp:
+    left, right = line.split()
+    left_list.append(int(left))
+    right_list.append(int(right))
 
-print(ans)
+left_list.sort()
+right_list.sort()
+right_list_frequencies = Counter(right_list)
+
+print("Part 1:", sum(abs(left_list[i] - right_list[i]) for i in range(len(inp))))
+print("Part 2:", sum(loc_id * right_list_frequencies[loc_id] for loc_id in left_list))
