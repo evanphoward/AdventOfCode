86a6783051f3f2ea89a6f6aee9274eb8c7f2cd8c
Evan Howard <evanhoward@college.harvard.edu>
2024-12-02 22:04:54 -0500

    Update default.py

--- MiscFiles/default.py
+++ MiscFiles/default.py
@@ -1,9 +1,10 @@
 from MiscFiles.library import *
 
 inp = get_input(0, 0).split("\n")
+# grid, nr nc = get_grid(get_input(0, 0))
 ans = 0
 
-for x in inp:
+for line in inp:
     continue
 
 print(ans)
